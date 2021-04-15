226bfefe0e8881e07f34ad297638e6c53fed397f
yxlei <99yx.lei@gmail.com>
2021-04-16 00:38:52 +0800

    add index and bootstrap

--- src/main/java/blog/ray/community/community/controller/IndexController.java
+++ src/main/java/blog/ray/community/community/controller/IndexController.java
@@ -3,15 +3,13 @@ package blog.ray.community.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
